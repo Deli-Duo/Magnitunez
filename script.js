19d4655a086722eefb7a80cac28af2dd9defd85f
VTmeta <vlamanno2004@gmail.com>
2023-03-29 16:14:50 -0400

    Finished search function

--- script.js
+++ script.js
@@ -8,42 +8,33 @@ const options = {
 
 const genres = document.getElementById('genres');
 const searchInput = document.getElementById('search');
+const searchResults = []
 const searchButton = document.getElementById('search-button');
 const recommendedButton = document.querySelector('#recommended');
 const historyButton = document.querySelector('#history');
 const likedSongsButton = document.querySelector('#liked');
-let topResults
+let topResult
+
 
 searchButton.addEventListener('click', async (e) => 
+
 {
+	e.preventDefault()
 	let searchValue = searchInput.value
 	searchValue = searchValue.replace(' ', "_")
-	e.preventDefault()
-	async function search() {
-	await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=` + searchValue, options)
-	.then(response =>response.json())
-	.then(data => {
+	localStorage.setItem('searchValue',JSON.stringify(searchValue))
 	
-		localStorage.setItem('song-names', JSON.stringify(data))
-			topResults = data
-		console.log(data)
-	
-	}
-		) 
-	.catch(err => console.error(err));
-	}
-	search();
-	// setTimeout(function () {
-	// 	window.location ="search.html"
-	// },1000)
+	setTimeout(function () {
+		window.location ="search.html"
+	},3000)
 	
 	
 })
 
 // Raven: Display recommended artists based on the most recently searched artist's Spotify ID
 // Adding placeholder variable for the fetch for now
-let id = ''
-likedSongsButton.addEventListener('click', (e) => {
-  e.preventDefault()
+// let id = ''
+// likedSongsButton.addEventListener('click', (e) => {
+//   e.preventDefault()
   
-})
+// })
--- search.js
+++ search.js
@@ -1,1 +1,32 @@
-console.log(JSON.parse(localStorage.getItem('song-names')))
+
+const results = document.querySelector('#search-results')
+
+const searchTitle = JSON.parse(localStorage.getItem('searchValue'))
+
+console.log(searchTitle)
+
+async function search() {
+	await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=` + searchTitle, options)
+	.then(response =>response.json())
+	.then(data => {
+
+		localStorage.setItem('song-names', JSON.stringify(data))
+		console.log(data.data)
+data.data.forEach(element => {
+    let li = document.createElement('li')
+    let div = document.createElement('div')
+    div.innerHTML = `<audio controls>
+    <source src="${element.preview}" type="audio/ogg">
+  Your browser does not support the audio element.
+  </audio>`
+    li.innerHTML = `<p>${element.title}"${element.artist.name}</p>`
+    li.append(div)
+    results.append(li)
+});	
+
+	
+	}
+		) 
+	.catch(err => console.error(err));
+	}
+	search();
