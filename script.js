69cec76304af2f1722f339afe5be08a5eff44d48
VTmeta <vlamanno2004@gmail.com>
2023-03-29 13:43:53 -0400

    changed api key

--- script.js
+++ script.js
@@ -7,20 +7,23 @@ const options = {
 };
 
 const genres = document.getElementById('genres');
-const search = document.getElementById('search');
+const searchInput = document.getElementById('search');
 const searchButton = document.getElementById('search-button');
 const recommendedButton = document.querySelector('#recommended');
 const historyButton = document.querySelector('#history');
 const likedSongsButton = document.querySelector('#liked');
 let topResults
 
-searchButton.addEventListener('click', (e) => 
+searchButton.addEventListener('click', async (e) => 
 {
+	let searchValue = searchInput.value
+	searchValue = searchValue.replace(' ', "_")
 	e.preventDefault()
-	async function search () {
-	await fetch(`https://spotify23.p.rapidapi.com/search/?q=${search.value}&type=multi&offset=0&limit=10&numberOfTopResults=5`, options)
-	.then(response => response.json())
+	async function search() {
+	await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=` + searchValue, options)
+	.then(response =>response.json())
 	.then(data => {
+	
 		localStorage.setItem('song-names', JSON.stringify(data))
 			topResults = data
 		console.log(data)
@@ -30,9 +33,9 @@ searchButton.addEventListener('click', (e) =>
 	.catch(err => console.error(err));
 	}
 	search();
-	setTimeout(function () {
-		window.location ="search.html"
-	},1000)
+	// setTimeout(function () {
+	// 	window.location ="search.html"
+	// },1000)
 	
 	
 })
